b37f5403a6f80b9c7156e2dbbfae688ec51978c9
tpdkrt <tpodko@gmail.com>
2024-11-30 22:27:16 +0100

    new image file with losses and small bug fix in nn.py

--- nn.py
+++ nn.py
@@ -94,7 +94,7 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        osses.append(loss.item())
+        losses.append(loss.item())
         
         if (i+1) % 10 == 0:
             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
